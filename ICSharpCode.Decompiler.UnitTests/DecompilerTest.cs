4ede6ec85b6edf585128cd9a7ddbf03490e2bc69
Richard Schneider <makaretu@gmail.com>
2011-02-23 15:49:18 +1300

    Unit tests using VS test tools
    Signed-off-by: Richard Schneider <makaretu@gmail.com>

---
+++ ICSharpCode.Decompiler.UnitTests/DecompilerTest.cs
@@ -0,0 +1,114 @@
+﻿using Decompiler;
+using Decompiler.Transforms;
+using ICSharpCode.Decompiler;
+using ICSharpCode.NRefactory.CSharp;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ICSharpCode.Decompiler
+{
+    public abstract class DecompilerTest
+    {
+        AssemblyDefinition targetAssembly;
+
+        /// <summary>
+        ///   Gets the assembly that will be used for decompilation.
+        /// </summary>
+        /// <value>
+        ///   The default <see cref="AssemblyDefinition"/> is the Decompile.UnitTests assembly.
+        /// </value>
+        public AssemblyDefinition TargetAssembly
+        {
+            get
+            {
+                if (targetAssembly == null)
+                    targetAssembly = AssemblyDefinition.ReadAssembly(Assembly.GetExecutingAssembly().Location);
+                return targetAssembly;
+            }
+            set
+            {
+                targetAssembly = value;
+            }
+        }
+
+        public string DecompileMethod(Type @class, string methodName)
+        {
+            var type = TargetAssembly.MainModule.GetType(@class.FullName.Replace('+', '/'));
+            if (type == null)
+                throw new Exception(string.Format("Can not locate type '{0}' in assembly '{1}'", @class.FullName, TargetAssembly.FullName));
+            var method = type.Methods.FirstOrDefault(m => m.Name == methodName);
+            if (method == null)
+                throw new Exception(string.Format("Can not locate method '{0}' in type '{1}'", methodName, type.FullName));
+
+            StringWriter source = new StringWriter();
+            AstBuilder decompiler = new AstBuilder(new DecompilerContext() { CurrentType = method.DeclaringType });
+            decompiler.AddMethod(method);
+            decompiler.GenerateCode(new ConciseTextOutput(source));
+
+            return source.ToString();
+        }
+
+        /// <summary>
+        ///   Don't generate newlines and indentation.  This is much easier for Assert.AreEqual(...)
+        /// </summary>
+        public sealed class ConciseTextOutput : ITextOutput
+        {
+            readonly TextWriter writer;
+
+            public ConciseTextOutput(TextWriter writer)
+            {
+                this.writer = writer;
+            }
+
+
+            public void Indent()
+            {
+            }
+
+            public void Unindent()
+            {
+            }
+
+            void WriteIndent()
+            {
+            }
+
+            public void Write(char ch)
+            {
+                writer.Write(ch);
+            }
+
+            public void Write(string text)
+            {
+                writer.Write(text);
+            }
+
+            public void WriteLine()
+            {
+            }
+
+            public void WriteDefinition(string text, object definition)
+            {
+                Write(text);
+            }
+
+            public void WriteReference(string text, object reference)
+            {
+                Write(text);
+            }
+
+            void ITextOutput.MarkFoldStart(string collapsedText, bool defaultCollapsed)
+            {
+            }
+
+            void ITextOutput.MarkFoldEnd()
+            {
+            }
+        }
+    }
+}
---
+++ ICSharpCode.Decompiler.UnitTests/Fault27.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ICSharpCode.Decompiler
+{
+    /// <summary>
+    ///   passing null to a method/constructor that has multiple signatures 
+    /// </summary>
+    /// <remarks>
+    ///   When the method has multiple signatures; such as foo(MyObject o)  and foo(string s), then code calling for with a null argument must have a cast.  
+    ///   Currently ILSpy will display foo(null) and it should be foo((string) null)  or foo((MyObject) null).
+    ///   
+    ///  I found this while looking at the .ctor for System.Xml.Resolvers.XmlPreloadedResolver.
+    /// </remarks>
+    [TestClass]
+    public class Fault27 : DecompilerTest
+    {
+        public class Sample
+        {
+            public void Foo(Sample o) { }
+            public void Foo(string s) { }
+            public void Foo(int v, string s) { }
+            public void Foo(float v, string s) { }
+            public void Bar(Sample o) { }
+
+            void Call1() { Foo((Sample)null); }
+            void Call2() { Foo((string)null); }
+            void Call3() { Foo(1, null); }
+            void Call4() { Foo(1.0f, null); }
+            void Call5() { Bar(null); }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("private void Call1(){this.Foo((Fault27.Sample)null);}", DecompileMethod(typeof(Sample), "Call1"));
+            Assert.AreEqual("private void Call2(){this.Foo((string)null);}", DecompileMethod(typeof(Sample), "Call2"));
+            Assert.AreEqual("private void Call3(){this.Foo(1, null);}", DecompileMethod(typeof(Sample), "Call3"));
+            Assert.AreEqual("private void Call4(){this.Foo(1f, null);}", DecompileMethod(typeof(Sample), "Call4"));
+            Assert.AreEqual("private void Call5(){this.Bar(null);}", DecompileMethod(typeof(Sample), "Call5"));
+        }
+    }
+}
