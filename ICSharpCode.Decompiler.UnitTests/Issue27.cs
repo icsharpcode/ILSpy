4cbaa1416b00c7dff8372dccc1c44bda1cd23d31
Richard Schneider <makaretu@gmail.com>
2011-02-24 12:05:15 +1300

    Use the name "Issue" instead of "Fault"

--- ICSharpCode.Decompiler.UnitTests/Issue27.cs
+++ ICSharpCode.Decompiler.UnitTests/Issue27.cs
@@ -16,7 +16,7 @@ namespace ICSharpCode.Decompiler
     ///  I found this while looking at the .ctor for System.Xml.Resolvers.XmlPreloadedResolver.
     /// </remarks>
     [TestClass]
-    public class Fault27 : DecompilerTest
+    public class Issue27 : DecompilerTest
     {
         public class Sample
         {
@@ -36,7 +36,7 @@ namespace ICSharpCode.Decompiler
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("private void Call1(){this.Foo((Fault27.Sample)null);}", DecompileMethod(typeof(Sample), "Call1"));
+            Assert.AreEqual("private void Call1(){this.Foo((Issue27.Sample)null);}", DecompileMethod(typeof(Sample), "Call1"));
             Assert.AreEqual("private void Call2(){this.Foo((string)null);}", DecompileMethod(typeof(Sample), "Call2"));
             Assert.AreEqual("private void Call3(){this.Foo(1, null);}", DecompileMethod(typeof(Sample), "Call3"));
             Assert.AreEqual("private void Call4(){this.Foo(1f, null);}", DecompileMethod(typeof(Sample), "Call4"));
