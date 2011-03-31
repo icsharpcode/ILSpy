964521c4d0ccccceb35332de85c00ea4a00fcd02
ReneDeLaGarza <rene.delagarza@gmail.com>
2011-03-31 13:34:14 -0700

    Edited ICSharpCode.Decompiler/ILAst/ILAstBuilder.cs via GitHub

--- ICSharpCode.Decompiler/ILAst/ILAstBuilder.cs
+++ ICSharpCode.Decompiler/ILAst/ILAstBuilder.cs
@@ -414,14 +414,14 @@ namespace ICSharpCode.Decompiler.ILAst
 				if (byteCode.StoreTo != null && byteCode.StoreTo.Count > 1) {
 					var locVars = byteCode.StoreTo;
 					// For each of the variables, find the location where it is loaded - there should be preciesly one
-					var loadedBy = locVars.Select(argVar => body.SelectMany(bc => bc.StackBefore).Where(s => s.LoadFrom == argVar).Single()).ToList();
+					var loadedBy = locVars.Select(argVar => body.Where(bc => bc.StackBefore != null).SelectMany(bc => bc.StackBefore).Where(s => s.LoadFrom == argVar).Single()).ToList();
 					// We now know that all the variables have a single load,
 					// Let's make sure that they have also a single store - us
 					if (loadedBy.All(slot => slot.PushedBy.Length == 1 && slot.PushedBy[0] == byteCode)) {
 						// Great - we can reduce everything into single variable
 						ILVariable tmpVar = new ILVariable() { Name = string.Format("expr_{0:X2}", byteCode.Offset), IsGenerated = true };
 						byteCode.StoreTo = new List<ILVariable>() { tmpVar };
-						foreach(ByteCode bc in body) {
+						foreach(ByteCode bc in body.Where(bc => bc.StackBefore!= null)) {
 							for (int i = 0; i < bc.StackBefore.Count; i++) {
 								// Is it one of the variable to be merged?
 								if (locVars.Contains(bc.StackBefore[i].LoadFrom)) {
