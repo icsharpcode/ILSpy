edf65929aed0d91dd21b85b5b7cc68e6cd157869
Arthur Wang <arthur200126@gmail.com>
2014-05-11 23:07:59 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 pause(){
   read -n 1 -p "Press any key to continue..."
 }
 xbuild /m ILSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Debug
 [ "$?" != "0" ] && pause && xbuild /m ILSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Release
-[ "$?" != "0" ] && pause
+[ "$?" != "0" ] && pause && exit 1
