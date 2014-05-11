e43ca40c9ac1e1f558f9f11b8f23dc17b2412913
Arthur Wang <arthur200126@gmail.com>
2014-05-11 23:07:13 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pause(){
+  read -n 1 -p "$1"
+}
+[ "$1" != "Debug" ] && [ "$1" != "Release" ] && echo -e "Please specify build type.\n  Usage: ./build.sh [ Debug | Release ]" > &2 && exit 1
+xbuild /m ILSpy.sln /p:Configuration=$1 "/p:Platform=Any CPU"
+[ "$?" != "0" ] && exit 1
