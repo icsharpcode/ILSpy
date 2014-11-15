f0382f6dbc471875adba2960cd7b57957fdfcb30
Siegfried Pammer <siegfriedpammer@gmail.com>
2014-11-15 10:48:33 +0100

    Add RemoveEmptyNamespace transform to get rid of <Module> definitions in all test cases

---
+++ ICSharpCode.Decompiler/Tests/Helpers/RemoveEmptyNamespace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.NRefactory.CSharp;
+
+namespace ICSharpCode.Decompiler.Tests.Helpers
+{
+	class RemoveEmptyNamespace : DepthFirstAstVisitor<object, object>
+	{
+		public override object VisitNamespaceDeclaration(NamespaceDeclaration namespaceDeclaration, object data)
+		{
+			if (namespaceDeclaration.FullName.Length == 0) {
+				namespaceDeclaration.Remove();
+				return null;
+			}
+			return base.VisitNamespaceDeclaration(namespaceDeclaration, data);
+		}
+
+		public void Run(AstNode node)
+		{
+			node.AcceptVisitor(this, null);
+		}
+	}
+}
