b33b5ca9bfce4e211b765a07f36d8ea218eddfdb
Daniel Grunwald <daniel@danielgrunwald.de>
2015-12-27 21:50:03 +0100

    .bat files: initialize git submodules if necessary

--- debugbuild.bat
+++ debugbuild.bat
@@ -1,3 +1,6 @@
+@if not exist "AvalonEdit\ICSharpCode.AvalonEdit.sln" (
+	git submodule update --init || exit /b 1
+)
 %windir%\microsoft.net\framework\v4.0.30319\msbuild /m ILSpy.sln /p:Configuration=Debug "/p:Platform=Any CPU"
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
