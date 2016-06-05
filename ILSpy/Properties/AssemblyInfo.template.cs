6a58560bbd864cb89adc1ed76f7ccb68d2e7a911
Siegfried Pammer <siegfriedpammer@gmail.com>
2016-06-06 00:29:19 +0900

    bump version to 2.4

--- ILSpy/Properties/AssemblyInfo.template.cs
+++ ILSpy/Properties/AssemblyInfo.template.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 internal static class RevisionClass
 {
 	public const string Major = "2";
-	public const string Minor = "3";
-	public const string Build = "2";
+	public const string Minor = "4";
+	public const string Build = "0";
 	public const string Revision = "$INSERTREVISION$";
 	public const string VersionName = null;
 	
