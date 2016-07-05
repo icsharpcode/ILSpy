cd42d1f36c1f37dc641e2c58bf2a3e8376f5fdc0
Calin Pirtea <pcalin@yahoo.com>
2016-07-05 18:56:34 +0800

    ... really?

--- ICSharpCode.Decompiler/DecompilerSettings.cs
+++ ICSharpCode.Decompiler/DecompilerSettings.cs
@@ -93,11 +93,11 @@ namespace ICSharpCode.Decompiler
 		/// Decompile automatic properties
 		/// </summary>
 		public bool AutomaticReadonlyProperties
-        {
+		{
 			get { return _automaticReadonlyProperties; }
 			set {
 				if (_automaticReadonlyProperties != value) {
-                    _automaticReadonlyProperties = value;
+					_automaticReadonlyProperties = value;
 					OnPropertyChanged("AutomaticReadonlyProperties");
 				}
 			}
