fc725e4ddebb5a404c66d11ef68418f94dbdf7ae
AraHaan <seandhunt_7@yahoo.com>
2016-07-12 01:48:25 -0500

    Changed some other things and added a comment.

--- ICSharpCode.Updater/Downloader.cs
+++ ICSharpCode.Updater/Downloader.cs
@@ -5,8 +5,14 @@ namespace ICSharpCode.Updater
 {
 	public class Downloader
 	{
-		public bool DownloadUpdate(String url, String path)
+		public bool DownloadUpdate(string url, string path)
 		{
+			/*
+				ILSpy Must Suply the URL to update itself. It Must be a constant one that never changes except for the content.
+				Why? To Ensure all versions of ILSpy after they include this to be able to get the latest one.
+				Who knows it might be possible to configure ILSpy to check for Nightly builds when on the stable one
+				and then have this download the Nightly build for them.
+			*/
 			bool result = StartDownload(url, path + "\\ILSpy_Update.zip");
 			if (result == true)
 			{
@@ -18,11 +24,11 @@ namespace ICSharpCode.Updater
 			}
 		}
 
-		private bool StartDownload(String url, String path)
+		private bool StartDownload(string url, string path)
 		{
 			try
 			{
-				String downloadconst = "0";
+				string downloadconst = "0";
 				WebClient webClient = new WebClient();
 				webClient.Headers["User-Agent"] = "Mozilla/5.0";
 				webClient.DownloadProgressChanged += delegate (object sender, DownloadProgressChangedEventArgs args)
