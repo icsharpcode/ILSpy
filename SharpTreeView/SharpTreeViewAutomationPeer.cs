9f3335fabf0e3b631a6775b1ada2f571b5ccb84b
Daniel Grunwald <daniel@danielgrunwald.de>
2016-12-03 19:35:42 +0100

    Fix code formatting.

--- SharpTreeView/SharpTreeViewAutomationPeer.cs
+++ SharpTreeView/SharpTreeViewAutomationPeer.cs
@@ -9,7 +9,7 @@ namespace ICSharpCode.TreeView
 {
 	class SharpTreeViewAutomationPeer : FrameworkElementAutomationPeer
 	{
-			internal SharpTreeViewAutomationPeer(SharpTreeView owner ): base(owner)
+		internal SharpTreeViewAutomationPeer(SharpTreeView owner ): base(owner)
 		{
 		}
 		//private SharpTreeView  SharpTreeView { get { return (SharpTreeView)base.Owner; } }
