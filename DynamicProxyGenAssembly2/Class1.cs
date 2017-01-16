94c6d93b3b384c8703e616607f49cf3c89c331e1
exyi <exyi@outlook.cz>
2017-01-16 19:51:43 +0100

    Implemented really simple GoToMetadataService backed by ILSpy

---
+++ DynamicProxyGenAssembly2/Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ICSharpCode.Decompiler;
+using ICSharpCode.ILSpy;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.MetadataAsSource;
+using Microsoft.CodeAnalysis.Text;
+using Mono.Cecil;
+
+namespace DynamicProxyGenAssembly2
+{
+	[ExportLanguageServiceFactory(typeof(IMetadataAsSourceService), LanguageNames.CSharp, layer: ServiceLayer.Host)]
+	public class ILSpyMetadataAsSourceServiceFactory : ILanguageServiceFactory
+	{
+		public ILanguageService CreateLanguageService(HostLanguageServices languageServices)
+		{
+			return new ILSpyMetadataAsSourceService();
+		}
+	}
+
+	public class ILSpyMetadataAsSourceService : IMetadataAsSourceService
+	{
+		
+		public async Task<Document> AddSourceToAsync(Document document, ISymbol symbol, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			var assembly = symbol.ContainingAssembly;
+			var assemblies = document.Project.MetadataReferences.OfType<PortableExecutableReference>().Select(p => p.FilePath)
+				.ToArray();
+			var moduleDef = LoadAssembly(assemblies.First(p => Path.GetFileNameWithoutExtension(p) == assembly.Name));
+			var output = new PlainTextOutput();
+			if (symbol.Kind == SymbolKind.NamedType)
+			{
+				var typeRef = moduleDef.GetType(symbol.ContainingNamespace.ToDisplayString(), symbol.MetadataName);
+				new CSharpLanguage().DecompileType(typeRef.Resolve(), output, new DecompilationOptions());
+			}
+
+
+			return document.WithText(SourceText.From(output.ToString()));
+		}
+
+		private static ModuleDefinition LoadAssembly(string fileName)
+		{
+			ReaderParameters p = new ReaderParameters();
+			//p.AssemblyResolver = new MyAssemblyResolver();
+
+			var module = ModuleDefinition.ReadModule(fileName, p);
+
+			if (true)
+			{
+				try
+				{
+					LoadSymbols(module, fileName);
+				}
+				catch (IOException)
+				{
+				}
+				catch (UnauthorizedAccessException)
+				{
+				}
+				catch (InvalidOperationException)
+				{
+					// ignore any errors during symbol loading
+				}
+			}
+			return module;
+		}
+
+		class MyAssemblyResolver : IAssemblyResolver
+		{
+			public AssemblyDefinition Resolve(string fullName)
+			{
+				throw new NotImplementedException();
+			}
+
+			public AssemblyDefinition Resolve(AssemblyNameReference name)
+			{
+				throw new NotImplementedException();
+			}
+
+			public AssemblyDefinition Resolve(string fullName, ReaderParameters parameters)
+			{
+				throw new NotImplementedException();
+			}
+
+			public AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		private static void LoadSymbols(ModuleDefinition module, string fileName)
+		{
+			if (!module.HasDebugHeader)
+			{
+				return;
+			}
+			byte[] headerBytes;
+			var debugHeader = module.GetDebugHeader(out headerBytes);
+			if (debugHeader.Type != 2)
+			{
+				// the debug type is not IMAGE_DEBUG_TYPE_CODEVIEW
+				return;
+			}
+			if (debugHeader.MajorVersion != 0 || debugHeader.MinorVersion != 0)
+			{
+				// the PDB type is not compatible with PdbReaderProvider. It is probably a Portable PDB
+				return;
+			}
+
+			// search for pdb in same directory as dll
+			string pdbName = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + ".pdb");
+			if (File.Exists(pdbName))
+			{
+				using (Stream s = File.OpenRead(pdbName))
+				{
+					module.ReadSymbols(new Mono.Cecil.Pdb.PdbReaderProvider().GetSymbolReader(module, s));
+				}
+				return;
+			}
+
+			// TODO: use symbol cache, get symbols from microsoft
+		}
+
+		
+	}
+}
---
+++ DynamicProxyGenAssembly2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DynamicProxyGenAssembly2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DynamicProxyGenAssembly2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("24da0ebd-1ebe-40d4-b17b-0ed84d059d47")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("609.0.8.8542")]
+[assembly: AssemblyFileVersion("609.0.8.8542")]
