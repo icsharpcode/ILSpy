e26af08a1cf260d6521dd8e830401dd5ab47fe91
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-08-31 11:56:30 +0200

    Move test projects to their own directory in \

--- ICSharpCode.Decompiler.Tests/TestCases/Correctness/Conversions.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/Correctness/Conversions.cs
@@ -16,7 +16,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-// #include "../../../Util/CSharpPrimitiveCast.cs"
+// #include "../../../ICSharpCode.Decompiler/Util/CSharpPrimitiveCast.cs"
 
 using System;
 using ICSharpCode.Decompiler.Util;
