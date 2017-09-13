15072606ab501e3a611430ace364436113fd3772
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-09-14 00:31:17 +0200

    Move UpdateAssemblyInfo to update-assemblyinfo.ps1

--- BuildTools/appveyor-install.ps1
+++ BuildTools/appveyor-install.ps1
@@ -1,17 +1,29 @@
-$majorVersion = 3;
-$minorVersion = 0;
-$revision = 0;
-$baseCommitHash = "d779383cb85003d6dabeb976f0845631e07bf463";
+$ErrorActionPreference = "Stop"
 
+$baseCommit = "d779383cb85003d6dabeb976f0845631e07bf463";
+$baseCommitRev = 1;
+
+$globalAssemblyInfoTemplateFile = "ILSpy/Properties/AssemblyInfo.template.cs";
+
+$versionParts = @{};
+Get-Content $globalAssemblyInfoTemplateFile | where { $_ -match 'string (\w+) = "?(\w+)"?;' } | foreach { $versionParts.Add($Matches[1], $Matches[2]) }
+
+$major = $versionParts.Major;
+$minor = $versionParts.Minor;
+$build = $versionParts.Build;
+$versionName = $versionParts.VersionName;
+
+if ($versionName -ne "null") {
+    $versionName = "-$versionName";
+}
 if ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
 	$branch = "-$env:APPVEYOR_REPO_BRANCH";
 } else {
 	$branch = "";
 }
 
-$build = [Int32]::Parse((git rev-list --count "$baseCommitHash..HEAD")) + 1;
+$revision = [Int32]::Parse((git rev-list --count "$baseCommit..HEAD")) + 1;
 
-$newVersion="$majorVersion.$minorVersion.$revision.$build";
-$env:ilspy_version_number=$newVersion;
-$env:appveyor_build_version="$newVersion$branch";
-appveyor UpdateBuild -Version "$newVersion$branch";
+$newVersion="$majorVersion.$minorVersion.$build.$revision";
+$env:appveyor_build_version="$newVersion$branch$versionName";
+appveyor UpdateBuild -Version "$newVersion$branch$versionName";
---
+++ BuildTools/update-assemblyinfo.ps1
@@ -0,0 +1,111 @@
+﻿$ErrorActionPreference = "Stop"
+
+$baseCommit = "d779383cb85003d6dabeb976f0845631e07bf463";
+$baseCommitRev = 1;
+
+$globalAssemblyInfoTemplateFile = "ILSpy/Properties/AssemblyInfo.template.cs";
+
+function Test-File([string]$filename) {
+    return [System.IO.File]::Exists( (Join-Path (Get-Location) $filename) );
+}
+
+function Test-Dir([string]$name) {
+    return [System.IO.Directory]::Exists( (Join-Path (Get-Location) $name) );
+}
+
+function gitVersion() {
+    if (-not (Test-Dir ".git")) {
+        return 1;
+    }
+    return [Int32]::Parse((git rev-list --count "$baseCommit..HEAD")) + $baseCommitRev;
+}
+
+function gitCommitHash() {
+    if (-not (Test-Dir ".git")) {
+        return "";
+    }
+    return (git rev-list "$baseCommit..HEAD") | Select -First 1;
+}
+
+function gitBranch() {
+    if (-not (Test-Dir ".git")) {
+        return "";
+    }
+
+    if ($env:APPVEYOR_REPO_BRANCH -ne $null) {
+        return $env:APPVEYOR_REPO_BRANCH;
+    } else {
+        return ((git branch --no-color).Split([System.Environment]::NewLine) | where { $_ -match "^\* " } | select -First 1).Substring(2);
+    }
+}
+
+$templateFiles = (
+	@{Input=$globalAssemblyInfoTemplateFile; Output="ILSpy/Properties/AssemblyInfo.cs"},
+	@{Input="ICSharpCode.Decompiler/Properties/AssemblyInfo.template.cs"; Output="ICSharpCode.Decompiler/Properties/AssemblyInfo.cs"},
+	@{Input="ILSpy/Properties/app.config.template"; Output = "ILSpy/app.config"}
+);
+[string]$mutexId = "ILSpyUpdateAssemblyInfo" + $PSScriptRoot.GetHashCode();
+[bool]$createdNew = $false;
+$mutex = New-Object System.Threading.Mutex($true, $mutexId, [ref]$createdNew);
+try {
+    if (-not $createdNew) {
+        try {
+		    $mutex.WaitOne(10000);
+	    } catch [System.Threading.AbandonedMutexException] {
+	    }
+        return 0;
+    }
+
+    if (-not (Test-File "ILSpy.sln")) {
+        Write-Host "Working directory must be the ILSpy repo root!";
+        return 2;
+    }
+
+    $versionParts = @{};
+    Get-Content $globalAssemblyInfoTemplateFile | where { $_ -match 'string (\w+) = "?(\w+)"?;' } | foreach { $versionParts.Add($Matches[1], $Matches[2]) }
+
+    $major = $versionParts.Major;
+    $minor = $versionParts.Minor;
+    $build = $versionParts.Build;
+    $versionName = $versionParts.VersionName;
+    $revision = gitVersion;
+    $branchName = gitBranch;
+    $gitCommitHash = gitCommitHash;
+
+    $postfixBranchName = "";
+    if ($branchName -ne "master") {
+        $postfixBranchName = "-$branchName";
+    }
+
+    if ($versionName -eq "null") {
+        $versionName = "";
+        $postfixVersionName = "";
+    } else {
+        $postfixVersionName = "-$versionName";
+    }
+
+    $fullVersionNumber = "$major.$minor.$build.$revision";
+    
+    foreach ($file in $templateFiles) {
+        [string]$in = (Get-Content $file.Input) -Join [System.Environment]::NewLine;
+
+		$out = $in.Replace('$INSERTVERSION$', $fullVersionNumber);
+		$out = $out.Replace('$INSERTMAJORVERSION$', $major);
+		$out = $out.Replace('$INSERTREVISION$', $minor);
+		$out = $out.Replace('$INSERTCOMMITHASH$', $gitCommitHash);
+		$out = $out.Replace('$INSERTSHORTCOMMITHASH$', $gitCommitHash.Substring(0, 8));
+		$out = $out.Replace('$INSERTDATE$', [System.DateTime]::Now.ToString("MM/dd/yyyy"));
+		$out = $out.Replace('$INSERTYEAR$', [System.DateTime]::Now.Year.ToString());
+		$out = $out.Replace('$INSERTBRANCHNAME$', $branchName);
+        $out = $out.Replace('$INSERTBRANCHPOSTFIX$', $postfixBranchName);
+		$out = $out.Replace('$INSERTVERSIONNAME$', $versionName);
+        $out = $out.Replace('$INSERTVERSIONNAMEPOSTFIX$', $postfixVersionName);
+
+        if (((Get-Content $file.Input) -Join [System.Environment]::NewLine) -ne $out) {
+            $out | Out-File $file.Output;
+        }
+    }
+} finally {
+    $mutex.ReleaseMutex();
+    $mutex.Dispose();
+}
