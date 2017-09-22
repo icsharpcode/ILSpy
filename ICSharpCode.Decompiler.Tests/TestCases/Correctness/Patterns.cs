6da80585872d939b3af4824a6cf87b99b35ebe10
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-09-22 22:05:21 +0200

    Add Patterns correctness tests; add missing file headers

---
+++ ICSharpCode.Decompiler.Tests/TestCases/Correctness/Patterns.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSharpCode.Decompiler.Tests.TestCases.Correctness
+{
+	/// <summary>
+	/// This file contains special cases of some patterns that cannot be tested in pretty tests.
+	/// </summary>
+	class Patterns
+	{
+		static void Main()
+		{
+			SimpleUsingNullStatement();
+		}
+
+		/// <summary>
+		/// Special case: Roslyn eliminates the try-finally altogether.
+		/// </summary>
+		public static void SimpleUsingNullStatement()
+		{
+			Console.WriteLine("before using");
+			using (null) {
+				Console.WriteLine("using (null)");
+			}
+			Console.WriteLine("after using");
+		}
+	}
+}
