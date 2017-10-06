2d2ca893e9d89ce016f63e8bba6ba9cb5958d6da
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-10-06 09:13:52 +0200

    ConditionDetection: Remove empty else-branches.

---
+++ ICSharpCode.Decompiler/IL/Instructions/ILInstructionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICSharpCode.Decompiler.IL
+{
+	public static class ILInstructionExtensions
+	{
+		/// <summary>
+		/// Determines whether a block only consists of nop instructions or is empty.
+		/// </summary>
+		public static bool IsNopBlock(this Block block, ILInstruction ignoreExitPoint = null)
+		{
+			if (block == null)
+				throw new ArgumentNullException(nameof(block));
+			return block.Children.Count(i => !(i is Nop) && i != ignoreExitPoint) == 0;
+		}
+	}
+}
