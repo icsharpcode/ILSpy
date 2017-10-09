33a7e2bf1d6f9450578d35fdcc751d168d25498b
mohe2015 <mohe2015@users.noreply.github.com>
2017-10-09 10:51:34 +0200

    Add more tests.

--- ICSharpCode.Decompiler.Tests/TestCases/Pretty/FixProxyCalls.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/Pretty/FixProxyCalls.cs
@@ -53,4 +53,66 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.ILPretty
 			return func(test);
 		}
 	}
+
+	internal class G
+	{
+		protected internal virtual void Test(string test)
+		{
+			string.Join(test, "fsdf");
+		}
+	}
+
+	internal class H : G
+	{
+		protected internal override void Test(string test)
+		{
+			Action<string> action = (Action<string>)delegate(string a) {
+				base.Test(a);
+			};
+			if (test.Equals(1)) {
+				throw new Exception("roslyn optimize is inlining the assignment which lets the test fail");
+			}
+			action(test);
+		}
+	}
+
+	internal class I
+	{
+		protected internal virtual void Test(int a)
+		{
+
+		}
+	}
+
+	internal class J : I
+	{
+		protected internal override void Test(int a)
+		{
+			Action action = (Action)delegate() {
+				base.Test(a);
+			};
+			if (a.Equals(1)) {
+				throw new Exception("roslyn optimize is inlining the assignment which lets the test fail");
+			}
+			action();
+
+		}
+	}
+
+	internal class K
+	{
+		protected internal virtual IEnumerable<int> Test(int p)
+		{
+			yield return p + 1;
+			yield return p + 2;
+		}
+	}
+
+	internal class L : K
+	{
+		protected internal override IEnumerable<int> Test(int p)
+		{
+			yield return base.Test(base.Test(0).GetEnumerator().Current).GetEnumerator().Current;
+		}
+	}
 }
