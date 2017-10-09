892c437e749bb2a8e02a22988f0233b5005033e7
mohe2015 <mohe2015@users.noreply.github.com>
2017-10-09 13:59:11 +0200

    working

--- ICSharpCode.Decompiler/IL/ControlFlow/AsyncAwaitDecompiler.cs
+++ ICSharpCode.Decompiler/IL/ControlFlow/AsyncAwaitDecompiler.cs
@@ -774,14 +774,14 @@ namespace ICSharpCode.Decompiler.IL.ControlFlow
 				return false;
 			if (!target.MatchLdThis())
 				return false;
-			if (field.MemberDefinition != awaiterField)
+			if (!field.MemberDefinition.Equals(awaiterField.MemberDefinition))
 				return false;
 			pos++;
 
 			// stfld awaiterField(ldloc this, default.value)
 			if (block.Instructions[pos].MatchStFld(out target, out field, out value)
 				&& target.MatchLdThis()
-				&& field.MemberDefinition == awaiterField
+				&& field.MemberDefinition.Equals(awaiterField.MemberDefinition)
 				&& value.OpCode == OpCode.DefaultValue)
 			{
 				pos++;
