9e5d4c10f76ca587cdbbc7532590fb03422e585d
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-10-12 23:13:03 +0200

    Fix array.Length == 0 in Roslyn.

--- ICSharpCode.Decompiler/IL/Transforms/ExpressionTransforms.cs
+++ ICSharpCode.Decompiler/IL/Transforms/ExpressionTransforms.cs
@@ -111,16 +111,6 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 			    && inst.Sign == Sign.Unsigned
 			    && (inst.Kind == ComparisonKind.GreaterThan || inst.Kind == ComparisonKind.LessThanOrEqual))
 			{
-				ILInstruction array;
-				if (inst.Left.MatchLdLen(StackType.I, out array)) {
-					// comp.unsigned(ldlen array > conv i4->i(ldc.i4 0))
-					// => comp(ldlen.i4 array > ldc.i4 0)
-					// This is a special case where the C# compiler doesn't generate conv.i4 after ldlen.
-					context.Step("comp(ldlen.i4 array > ldc.i4 0)", inst);
-					inst.InputType = StackType.I4;
-					inst.Left.ReplaceWith(new LdLen(StackType.I4, array) { ILRange = inst.Left.ILRange });
-					inst.Right = rightWithoutConv;
-				}
 				if (inst.Kind == ComparisonKind.GreaterThan) {
 					context.Step("comp.unsigned(left > ldc.i4 0) => comp(left != ldc.i4 0)", inst);
 					inst.Kind = ComparisonKind.Inequality;
@@ -131,6 +121,16 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 					inst.Kind = ComparisonKind.Equality;
 					VisitComp(inst);
 					return;
+				}
+			} else if (rightWithoutConv.MatchLdcI4(0) && inst.Kind.IsEqualityOrInequality()) {
+				if (inst.Left.MatchLdLen(StackType.I, out ILInstruction array)) {
+					// comp.unsigned(ldlen array == conv i4->i(ldc.i4 0))
+					// => comp(ldlen.i4 array == ldc.i4 0)
+					// This is a special case where the C# compiler doesn't generate conv.i4 after ldlen.
+					context.Step("comp(ldlen.i4 array == ldc.i4 0)", inst);
+					inst.InputType = StackType.I4;
+					inst.Left.ReplaceWith(new LdLen(StackType.I4, array) { ILRange = inst.Left.ILRange });
+					inst.Right = rightWithoutConv;
 				}
 			}
 
