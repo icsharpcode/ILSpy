71f85d8e1de49f5dab298dd0b22fbef5ea97bbbe
Daniel Grunwald <daniel@danielgrunwald.de>
2017-10-13 13:04:11 +0200

    Move DelegateConstruction test into namespace.

--- ICSharpCode.Decompiler.Tests/TestCases/Pretty/DelegateConstruction.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/Pretty/DelegateConstruction.cs
@@ -20,177 +20,180 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class DelegateConstruction
+namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 {
-	private class InstanceTests
+	public static class DelegateConstruction
 	{
-		public Action CaptureOfThis()
+		private class InstanceTests
 		{
-			return delegate {
-				this.CaptureOfThis();
-			};
-		}
-		
-		public Action CaptureOfThisAndParameter(int a)
-		{
-			return delegate {
-				this.CaptureOfThisAndParameter(a);
-			};
-		}
-		
-		public Action CaptureOfThisAndParameterInForEach(int a)
-		{
-			foreach (int item in Enumerable.Empty<int>()) {
-				if (item > 0) {
-					return delegate {
-						this.CaptureOfThisAndParameter(item + a);
-					};
+			public Action CaptureOfThis()
+			{
+				return delegate {
+					this.CaptureOfThis();
+				};
+			}
+
+			public Action CaptureOfThisAndParameter(int a)
+			{
+				return delegate {
+					this.CaptureOfThisAndParameter(a);
+				};
+			}
+
+			public Action CaptureOfThisAndParameterInForEach(int a)
+			{
+				foreach (int item in Enumerable.Empty<int>()) {
+					if (item > 0) {
+						return delegate {
+							this.CaptureOfThisAndParameter(item + a);
+						};
+					}
+				}
+				return null;
+			}
+
+			public Action CaptureOfThisAndParameterInForEachWithItemCopy(int a)
+			{
+				foreach (int item in Enumerable.Empty<int>()) {
+					int copyOfItem = item;
+					if (item > 0) {
+						return delegate {
+							this.CaptureOfThisAndParameter(item + a + copyOfItem);
+						};
+					}
+				}
+				return null;
+			}
+
+			public void LambdaInForLoop()
+			{
+				for (int i = 0; i < 100000; i++) {
+					this.Bar(() => this.Foo());
 				}
 			}
-			return null;
-		}
-		
-		public Action CaptureOfThisAndParameterInForEachWithItemCopy(int a)
-		{
-			foreach (int item in Enumerable.Empty<int>()) {
-				int copyOfItem = item;
-				if (item > 0) {
-					return delegate {
-						this.CaptureOfThisAndParameter(item + a + copyOfItem);
-					};
-				}
+
+			public int Foo()
+			{
+				return 0;
 			}
-			return null;
-		}
-		
-		public void LambdaInForLoop()
-		{
-			for (int i = 0; i < 100000; i++) {
-				this.Bar(() => this.Foo());
+
+			public void Bar(Func<int> f)
+			{
 			}
 		}
-		
-		public int Foo()
-		{
-			return 0;
-		}
-		
-		public void Bar(Func<int> f)
+
+		public static void Test(this string a)
 		{
 		}
-	}
 
-	public static void Test(this string a)
-	{
-	}
+		public static Action<string> ExtensionMethodUnbound()
+		{
+			return new Action<string>(DelegateConstruction.Test);
+		}
 
-	public static Action<string> ExtensionMethodUnbound()
-	{
-		return new Action<string>(DelegateConstruction.Test);
-	}
+		public static Action ExtensionMethodBound()
+		{
+			return new Action("abc".Test);
+		}
 
-	public static Action ExtensionMethodBound()
-	{
-		return new Action("abc".Test);
-	}
+		public static Action ExtensionMethodBoundOnNull()
+		{
+			return new Action(((string)null).Test);
+		}
 
-	public static Action ExtensionMethodBoundOnNull()
-	{
-		return new Action(((string)null).Test);
-	}
+		public static object StaticMethod()
+		{
+			return new Func<Action>(DelegateConstruction.ExtensionMethodBound);
+		}
 
-	public static object StaticMethod()
-	{
-		return new Func<Action>(DelegateConstruction.ExtensionMethodBound);
-	}
+		public static object InstanceMethod()
+		{
+			return new Func<string>("hello".ToUpper);
+		}
 
-	public static object InstanceMethod()
-	{
-		return new Func<string>("hello".ToUpper);
-	}
+		public static object InstanceMethodOnNull()
+		{
+			return new Func<string>(((string)null).ToUpper);
+		}
 
-	public static object InstanceMethodOnNull()
-	{
-		return new Func<string>(((string)null).ToUpper);
-	}
+		public static List<Action<int>> AnonymousMethodStoreWithinLoop()
+		{
+			List<Action<int>> list = new List<Action<int>>();
+			for (int i = 0; i < 10; i++) {
+				int counter;
+				list.Add(delegate(int x) {
+					counter = x;
+				});
+			}
+			return list;
+		}
 
-	public static List<Action<int>> AnonymousMethodStoreWithinLoop()
-	{
-		List<Action<int>> list = new List<Action<int>>();
-		for (int i = 0; i < 10; i++) {
+		public static List<Action<int>> AnonymousMethodStoreOutsideLoop()
+		{
+			List<Action<int>> list = new List<Action<int>>();
 			int counter;
-			list.Add(delegate(int x) {
-			         	counter = x;
-			         });
+			for (int i = 0; i < 10; i++) {
+				list.Add(delegate(int x) {
+					counter = x;
+				});
+			}
+			return list;
 		}
-		return list;
-	}
 
-	public static List<Action<int>> AnonymousMethodStoreOutsideLoop()
-	{
-		List<Action<int>> list = new List<Action<int>>();
-		int counter;
-		for (int i = 0; i < 10; i++) {
-			list.Add(delegate(int x) {
-			         	counter = x;
-			         });
+		public static Action StaticAnonymousMethodNoClosure()
+		{
+			return delegate {
+				Console.WriteLine();
+			};
 		}
-		return list;
-	}
 
-	public static Action StaticAnonymousMethodNoClosure()
-	{
-		return delegate {
-			Console.WriteLine();
-		};
-	}
-
-	public static void NameConflict()
-	{
-		// i is captured variable,
-		// j is parameter in anonymous method
-		// l is local in anonymous method,
-		// k is local in main method
-		// Ensure that the decompiler doesn't introduce name conflicts
-		List<Action<int>> list = new List<Action<int>>();
-		for (int k = 0; k < 10; k++) {
-			int i;
-			for (i = 0; i < 10; i++) {
-				list.Add(delegate(int j) {
+		public static void NameConflict()
+		{
+			// i is captured variable,
+			// j is parameter in anonymous method
+			// l is local in anonymous method,
+			// k is local in main method
+			// Ensure that the decompiler doesn't introduce name conflicts
+			List<Action<int>> list = new List<Action<int>>();
+			for (int k = 0; k < 10; k++) {
+				int i;
+				for (i = 0; i < 10; i++) {
+					list.Add(delegate(int j) {
 						for (int l = 0; l < i; l += j) {
 							Console.WriteLine();
 						}
 					});
+				}
 			}
 		}
-	}
 
-	public static void NameConflict2(int j)
-	{
-		List<Action<int>> list = new List<Action<int>>();
-		for (int k = 0; k < 10; k++) {
-			list.Add(delegate(int i) {
+		public static void NameConflict2(int j)
+		{
+			List<Action<int>> list = new List<Action<int>>();
+			for (int k = 0; k < 10; k++) {
+				list.Add(delegate(int i) {
 					Console.WriteLine(i);
 				});
-		}
-	}
-
-	public static Action<int> NameConflict3(int i)
-	{
-		return delegate(int j) {
-			for (int k = 0; k < j; k++) {
-				Console.WriteLine(k);
 			}
-		};
-	}
-	
-	public static Func<int, Func<int, int>> CurriedAddition(int a)
-	{
-		return (int b) => (int c) => a + b + c;
-	}
-	
-	public static Func<int, Func<int, Func<int, int>>> CurriedAddition2(int a)
-	{
-		return (int b) => (int c) => (int d) => a + b + c + d;
+		}
+
+		public static Action<int> NameConflict3(int i)
+		{
+			return delegate(int j) {
+				for (int k = 0; k < j; k++) {
+					Console.WriteLine(k);
+				}
+			};
+		}
+
+		public static Func<int, Func<int, int>> CurriedAddition(int a)
+		{
+			return (int b) => (int c) => a + b + c;
+		}
+
+		public static Func<int, Func<int, Func<int, int>>> CurriedAddition2(int a)
+		{
+			return (int b) => (int c) => (int d) => a + b + c + d;
+		}
 	}
 }
