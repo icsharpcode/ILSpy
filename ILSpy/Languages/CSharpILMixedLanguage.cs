25cdf7d16a0abb764b9496fe12c7371ce66e1ccb
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-10-14 10:27:31 +0200

    Add CSharpILMixedLanguage

---
+++ ILSpy/Languages/CSharpILMixedLanguage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.Disassembler;
+using Mono.Cecil;
+
+namespace ICSharpCode.ILSpy
+{
+	[Export(typeof(Language))]
+	class CSharpILMixedLanguage : Language
+	{
+		private readonly bool detectControlStructure = true;
+
+		public override string Name => "IL with C#";
+
+		public override string FileExtension => ".il";
+
+		public override void DecompileMethod(MethodDefinition method, ITextOutput output, DecompilationOptions options)
+		{
+			var dis = new ReflectionDisassembler(output, detectControlStructure, options.CancellationToken);
+			dis.DisassembleMethod(method);
+		}
+	}
+}
