d412de58b952bade5fef290dfe822fdb20e252dc
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-11-30 23:21:32 +0100

    Commit first test case.

---
+++ ICSharpCode.Decompiler.Tests/TestCases/Correctness/FloatingPointArithmetic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ICSharpCode.Decompiler.Tests.TestCases.Correctness
+{
+	class FloatingPointArithmetic
+	{
+		public static int Main(string[] args)
+		{
+			Issue999();
+			return 0;
+		}
+
+		static void Issue999()
+		{
+			for (float i = -10f; i <= 10f; i += 0.01f) 
+				Console.WriteLine("{1:R}: {0:R}", M(i), i);
+		}
+
+		static float M(float v)
+		{
+			return 0.99f * v + 0.01f;
+		}
+	}
+}
