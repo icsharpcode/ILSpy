14f9577a815bab03170df05fad97280eb5c0442d
MikeFH <MikeFH@users.noreply.github.com>
2018-01-15 22:17:41 +0100

    Fix lines starting with spaces

--- ICSharpCode.Decompiler.Tests/TestCases/Pretty/AsyncMain.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/Pretty/AsyncMain.cs
@@ -4,11 +4,11 @@ using System.Threading.Tasks;
 namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 {
 	public class AsyncMain
-    {
+	{
 		public static async Task Main(string[] args)
 		{
 			await Task.Delay(1000);
 			Console.WriteLine("Hello Wolrd!");
 		}
-    }
+	}
 }
