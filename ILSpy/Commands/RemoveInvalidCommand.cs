653a117c857250d8d614b18c1676a6123b69e519
Berman, Charles <Charles.Heath.Berman@gmail.com>
2018-03-13 12:46:51 -0400

    Added Ability to remove invalid nodes.

---
+++ ILSpy/Commands/RemoveInvalidCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.TreeView;
+
+namespace ICSharpCode.ILSpy.Commands
+{
+	[ExportMainMenuCommand(Menu = "_File", Header = "Remove Invalid", MenuCategory = "Remove", MenuOrder = 2.6)]
+
+	sealed class RemoveInvalidCommand:SimpleCommand
+	{
+		private IEnumerable<LoadedAssembly> InvalidAssemblys()
+		{
+			return MainWindow.Instance?.CurrentAssemblyList?.assemblies.Where(o => o.HasLoadError);
+		}
+
+		private List<SharpTreeNode> GetNodes()
+		{
+			var result = InvalidAssemblys()
+				.Select(assm =>
+					MainWindow.Instance.AssemblyListTreeNode.Children.FirstOrDefault(n => n.ToString().Equals(assm.FileName)))
+				.Where(o => o != null)
+				.ToList();
+			return result;
+		}
+		public override bool CanExecute(object parameter)
+		{
+			var invalid = InvalidAssemblys();
+			if (invalid == null)
+				return false;
+			return invalid.Any();
+		}
+
+		public override void Execute(object parameter)
+		{
+			var nodes = GetNodes();
+			for (var i = 0; i < nodes.Count; i++) {
+				var node = nodes[i];
+				if(node.CanDelete())
+					node.Delete();
+			}
+		}
+	}
+}
