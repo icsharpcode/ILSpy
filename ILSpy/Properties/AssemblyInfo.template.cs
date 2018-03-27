9f8b9078a7f05d3d224cfb23b53251bf29fa25e8
Siegfried Pammer <siegfriedpammer@gmail.com>
2018-03-28 01:20:42 +0200

    Set version to 4.0-alpha1

--- ILSpy/Properties/AssemblyInfo.template.cs
+++ ILSpy/Properties/AssemblyInfo.template.cs
@@ -38,11 +38,11 @@ using System.Diagnostics.CodeAnalysis;
 
 internal static class RevisionClass
 {
-	public const string Major = "3";
-	public const string Minor = "1";
+	public const string Major = "4";
+	public const string Minor = "0";
 	public const string Build = "0";
 	public const string Revision = "$INSERTREVISION$";
-	public const string VersionName = "beta1";
+	public const string VersionName = "alpha1";
 	
 	public const string FullVersion = Major + "." + Minor + "." + Build + ".$INSERTREVISION$$INSERTBRANCHPOSTFIX$$INSERTVERSIONNAMEPOSTFIX$";
 }
