497de76a2fb91e8964c86f8f51c18b73166922bb
Siegfried Pammer <siegfriedpammer@gmail.com>
2018-05-26 21:16:59 +0200

    Update test case for #1145

--- ICSharpCode.Decompiler.Tests/TestCases/ILPretty/Issue1145.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/ILPretty/Issue1145.cs
@@ -1,7 +1,12 @@
 using System;
 
+public sealed class EvType : MulticastDelegate
+{
+
+}
+
 [Serializable]
 public class OwningClass
 {
-	public event Action EvName;
+	public event EvType EvName;
 }
