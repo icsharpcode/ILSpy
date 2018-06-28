1f04729491b4f5cb337725a0e7bc207e8f0f3b31
Siegfried Pammer <siegfriedpammer@gmail.com>
2018-06-28 14:00:09 +0200

    IParameter.Attributes should always be non-null.

--- ICSharpCode.Decompiler/TypeSystem/Implementation/DefaultParameter.cs
+++ ICSharpCode.Decompiler/TypeSystem/Implementation/DefaultParameter.cs
@@ -43,6 +43,7 @@ namespace ICSharpCode.Decompiler.TypeSystem.Implementation
 				throw new ArgumentNullException("name");
 			this.type = type;
 			this.name = name;
+			this.attributes = EmptyList<IAttribute>.Instance;
 		}
 		
 		public DefaultParameter(IType type, string name, IParameterizedMember owner = null, IReadOnlyList<IAttribute> attributes = null,
@@ -55,7 +56,7 @@ namespace ICSharpCode.Decompiler.TypeSystem.Implementation
 			this.type = type;
 			this.name = name;
 			this.owner = owner;
-			this.attributes = attributes;
+			this.attributes = attributes ?? EmptyList<IAttribute>.Instance;
 			this.isRef = isRef;
 			this.isOut = isOut;
 			this.isParams = isParams;
