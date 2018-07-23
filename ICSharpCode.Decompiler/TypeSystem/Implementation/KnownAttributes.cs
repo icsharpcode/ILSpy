2d842a596a7435774e4a7f60aa659c47eb50af26
Siegfried Pammer <siegfriedpammer@gmail.com>
2018-07-23 22:36:24 +0200

    Add KnownAttribute.CallerMemberName, CallerFilePath and CallerLineNumber

--- ICSharpCode.Decompiler/TypeSystem/Implementation/KnownAttributes.cs
+++ ICSharpCode.Decompiler/TypeSystem/Implementation/KnownAttributes.cs
@@ -78,6 +78,9 @@ namespace ICSharpCode.Decompiler.TypeSystem
 		ParamArray,
 		In,
 		Out,
+		CallerMemberName,
+		CallerFilePath,
+		CallerLineNumber,
 
 		// Marshalling attributes:
 		MarshalAs,
@@ -130,6 +133,9 @@ namespace ICSharpCode.Decompiler.TypeSystem
 			new TopLevelTypeName("System", nameof(ParamArrayAttribute)),
 			new TopLevelTypeName("System.Runtime.InteropServices", nameof(InAttribute)),
 			new TopLevelTypeName("System.Runtime.InteropServices", nameof(OutAttribute)),
+			new TopLevelTypeName("System.Runtime.CompilerServices", nameof(CallerMemberNameAttribute)),
+			new TopLevelTypeName("System.Runtime.CompilerServices", nameof(CallerFilePathAttribute)),
+			new TopLevelTypeName("System.Runtime.CompilerServices", nameof(CallerLineNumberAttribute)),
 			// Marshalling attributes:
 			new TopLevelTypeName("System.Runtime.InteropServices", nameof(MarshalAsAttribute)),
 			// Security attributes:
