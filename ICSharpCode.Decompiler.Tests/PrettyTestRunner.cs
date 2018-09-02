ab9397d0990b14b6e3a99f4d4abfa84c4a8234c1
Chicken-Bones <mehvids@gmail.com>
2018-09-02 13:16:46 +1000

    Add ReduceNestingTransform

--- ICSharpCode.Decompiler.Tests/PrettyTestRunner.cs
+++ ICSharpCode.Decompiler.Tests/PrettyTestRunner.cs
@@ -128,6 +128,12 @@ namespace ICSharpCode.Decompiler.Tests
 		}
 
 		[Test]
+		public void ReduceNesting([ValueSource("defaultOptions")] CSharpCompilerOptions cscOptions)
+		{
+			RunForLibrary(cscOptions: cscOptions);
+		}
+
+		[Test]
 		public void DelegateConstruction([ValueSource("defaultOptions")] CSharpCompilerOptions cscOptions)
 		{
 			RunForLibrary(cscOptions: cscOptions);
