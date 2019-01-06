008a3c71e5a8d1dccf777e7b3326bd07a2baf7c9
Siegfried Pammer <siegfriedpammer@gmail.com>
2019-01-06 10:34:37 +0100

    DelegateConstruction: - add support for unknown delegate construction pattern. The method is compiler-generated, but has no compiler-generated name, we should be able to infer lambda usage from anonymous types used in the signature (i. e., the method can only be used as lambda, because that's where type names are optional). - Add CombineExitsTransform in DelegateConstruction: this allows combining lambdas with multiple return statements into a single expression (as it is expected by query expressions).

---
+++ ICSharpCode.Decompiler/IL/Transforms/CombineExitsTransform.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICSharpCode.Decompiler.IL.Transforms
+{
+	class CombineExitsTransform : IILTransform
+	{
+		public void Run(ILFunction function, ILTransformContext context)
+		{
+			if (!(function.Body is BlockContainer container && container.Blocks.Count == 1))
+				return;
+			var block = container.EntryPoint;
+			if (block.Kind != BlockKind.ControlFlow)
+				return;
+			if (!(block.Instructions.SecondToLastOrDefault() is IfInstruction ifInst && block.Instructions.LastOrDefault() is Leave leave))
+				return;
+			if (!ifInst.FalseInst.MatchNop())
+				return;
+			if (!(Block.Unwrap(ifInst.TrueInst) is Leave leave2))
+				return;
+			if (!(leave.IsLeavingFunction && leave2.IsLeavingFunction))
+				return;
+			if (leave.Value.MatchNop() || leave2.Value.MatchNop())
+				return;
+			IfInstruction value = new IfInstruction(ifInst.Condition, leave.Value, leave2.Value);
+			Leave combinedLeave = new Leave(leave.TargetContainer, value);
+			ifInst.ReplaceWith(combinedLeave);
+			block.Instructions.RemoveAt(combinedLeave.ChildIndex + 1);
+		}
+	}
+}
