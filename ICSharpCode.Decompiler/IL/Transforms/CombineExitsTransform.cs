d22e2ac677aa86ca5be2c84c0d9b9ecd2bfc14c9
Siegfried Pammer <siegfriedpammer@gmail.com>
2019-01-26 20:25:37 +0100

    Add header and comments describing transformation. Add missing ILRanges.

--- ICSharpCode.Decompiler/IL/Transforms/CombineExitsTransform.cs
+++ ICSharpCode.Decompiler/IL/Transforms/CombineExitsTransform.cs
@@ -1,6 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) 2019 Siegfried Pammer
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
 namespace ICSharpCode.Decompiler.IL.Transforms
 {
@@ -13,18 +27,24 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 			var block = container.EntryPoint;
 			if (block.Kind != BlockKind.ControlFlow)
 				return;
-			if (!(block.Instructions.SecondToLastOrDefault() is IfInstruction ifInst && block.Instructions.LastOrDefault() is Leave leave2))
+			if (!(block.Instructions.SecondToLastOrDefault() is IfInstruction ifInst && block.Instructions.LastOrDefault() is Leave leaveElse))
 				return;
 			if (!ifInst.FalseInst.MatchNop())
 				return;
 			if (!(Block.Unwrap(ifInst.TrueInst) is Leave leave))
 				return;
-			if (!(leave.IsLeavingFunction && leave2.IsLeavingFunction))
+			if (!(leave.IsLeavingFunction && leaveElse.IsLeavingFunction))
 				return;
-			if (leave.Value.MatchNop() || leave2.Value.MatchNop())
+			if (leave.Value.MatchNop() || leaveElse.Value.MatchNop())
 				return;
-			IfInstruction value = new IfInstruction(ifInst.Condition, leave.Value, leave2.Value);
-			Leave combinedLeave = new Leave(leave.TargetContainer, value);
+			// if (cond) {
+			//   leave (value)
+			// }
+			// leave (elseValue)
+			// =>
+			// leave (if (cond) value else elseValue)
+			IfInstruction value = new IfInstruction(ifInst.Condition, leave.Value, leaveElse.Value) { ILRange = ifInst.ILRange };
+			Leave combinedLeave = new Leave(leave.TargetContainer, value) { ILRange = leaveElse.ILRange };
 			ifInst.ReplaceWith(combinedLeave);
 			block.Instructions.RemoveAt(combinedLeave.ChildIndex + 1);
 		}
