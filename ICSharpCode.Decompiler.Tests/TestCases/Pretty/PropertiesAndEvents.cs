ae8065b1c09a5e4ca43d374c94d520958ff67941
Siegfried Pammer <siegfriedpammer@gmail.com>
2019-02-17 17:20:55 +0100

    Merge PropertiesAndEvents tests.

--- ICSharpCode.Decompiler.Tests/TestCases/Pretty/PropertiesAndEvents.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/Pretty/PropertiesAndEvents.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Text;
 
 namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 {
@@ -127,6 +146,20 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 			private set;
 		}
 
+		public int AutomaticProperty {
+			get;
+			set;
+		}
+
+		public int CustomProperty {
+			get {
+				return AutomaticProperty;
+			}
+			set {
+				AutomaticProperty = value;
+			}
+		}
+
 		private object Issue1221 {
 			set {
 				issue1221 = value;
@@ -165,6 +198,26 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 			remove {
 				AutomaticEvent -= value;
 			}
+		}
+
+		public int Getter(StringBuilder b)
+		{
+			return b.Length;
+		}
+
+		public void Setter(StringBuilder b)
+		{
+			b.Capacity = 100;
+		}
+
+		public char IndexerGetter(StringBuilder b)
+		{
+			return b[50];
+		}
+
+		public void IndexerSetter(StringBuilder b)
+		{
+			b[42] = 'b';
 		}
 	}
 }
