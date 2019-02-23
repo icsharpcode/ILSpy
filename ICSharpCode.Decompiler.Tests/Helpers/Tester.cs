5c76b66a20b21ecf6e967fd0e4418c1acaa769ed
Siegfried Pammer <siegfriedpammer@gmail.com>
2019-02-23 17:59:45 +0100

    Add netstandard.dll to Roslyn defaultReferences.

--- ICSharpCode.Decompiler.Tests/Helpers/Tester.cs
+++ ICSharpCode.Decompiler.Tests/Helpers/Tester.cs
@@ -184,9 +184,11 @@ namespace ICSharpCode.Decompiler.Tests.Helpers
 		static readonly Lazy<IEnumerable<MetadataReference>> defaultReferences = new Lazy<IEnumerable<MetadataReference>>(delegate {
 			string refAsmPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86),
 				@"Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5");
+			string thisAsmPath = Path.GetDirectoryName(typeof(Tester).Assembly.Location);
 			return new[]
 			{
 					MetadataReference.CreateFromFile(Path.Combine(refAsmPath, "mscorlib.dll")),
+					MetadataReference.CreateFromFile(Path.Combine(thisAsmPath, "netstandard.dll")),
 					MetadataReference.CreateFromFile(Path.Combine(refAsmPath, "System.dll")),
 					MetadataReference.CreateFromFile(Path.Combine(refAsmPath, "System.Core.dll")),
 					MetadataReference.CreateFromFile(Path.Combine(refAsmPath, @"Facades\System.Runtime.dll")),
