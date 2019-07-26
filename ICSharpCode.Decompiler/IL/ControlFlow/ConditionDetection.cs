6c0216bbb9042c55ebee854d32d54afd25090747
Daniel Grunwald <daniel@danielgrunwald.de>
2019-07-26 17:13:47 +0200

    Fix assertion in ReduceNestingTransform: after copying the exitInst, it was possible that the old copy of the exitInst became unreachable.

--- ICSharpCode.Decompiler/IL/ControlFlow/ConditionDetection.cs
+++ ICSharpCode.Decompiler/IL/ControlFlow/ConditionDetection.cs
@@ -354,7 +354,7 @@ namespace ICSharpCode.Decompiler.IL.ControlFlow
 			//assert then block terminates
 			var trueExitInst = GetExit(ifInst.TrueInst);
 			var exitInst = GetExit(block);
-			context.Step("Negate if for desired branch "+trueExitInst, ifInst);
+			context.Step($"InvertIf at IL_{ifInst.StartILOffset:x4}", ifInst);
 			
 			//if the then block terminates, else blocks are redundant, and should not exist
 			Debug.Assert(IsEmpty(ifInst.FalseInst));
