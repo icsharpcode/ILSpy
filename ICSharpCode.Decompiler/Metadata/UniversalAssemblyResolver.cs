22fa104ed6b64a96986f126c1a886d9152840810
Siegfried Pammer <siegfriedpammer@gmail.com>
2019-08-19 09:14:52 +0200

    UniversalAssemblyResolver: Unix can be either .NET Core or Mono, hence put the check last.

--- ICSharpCode.Decompiler/Metadata/UniversalAssemblyResolver.cs
+++ ICSharpCode.Decompiler/Metadata/UniversalAssemblyResolver.cs
@@ -35,10 +35,10 @@ namespace ICSharpCode.Decompiler.Metadata
 			// ever need this...
 			if (Type.GetType("Mono.Runtime") != null)
 				decompilerRuntime = DecompilerRuntime.Mono;
-			else if (Environment.OSVersion.Platform == PlatformID.Unix)
-				decompilerRuntime = DecompilerRuntime.Mono;
 			else if (typeof(object).Assembly.GetName().Name == "System.Private.CoreLib")
 				decompilerRuntime = DecompilerRuntime.NETCoreApp;
+			else if (Environment.OSVersion.Platform == PlatformID.Unix)
+				decompilerRuntime = DecompilerRuntime.Mono;
 		}
 
 		DotNetCorePathFinder dotNetCorePathFinder;
