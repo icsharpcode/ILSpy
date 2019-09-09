f51f20eb35f5f10d639cfb4a1a07f0440705b64f
Siegfried Pammer <siegfriedpammer@gmail.com>
2019-09-09 23:42:33 +0200

    Enable SharpTreeView to use Viewbox as icon container.

---
+++ SharpTreeView/IconDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ICSharpCode.TreeView
+{
+	class IconDataTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate ImageDataTemplate { get; set; }
+
+		public DataTemplate VectorDataTemplate { get; set; }
+
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			if (item is ImageSource)
+				return ImageDataTemplate;
+			if (item is DrawingGroup)
+				return VectorDataTemplate;
+
+			return base.SelectTemplate(item, container);
+		}
+	}
+}
