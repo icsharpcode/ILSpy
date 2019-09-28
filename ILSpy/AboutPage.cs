9c591fef6303efb5bc2a49e36a3dc3cc3eff60ba
Christoph Wille <christoph.wille@gmail.com>
2019-09-28 14:36:34 +0200

    Update to latest MEF NuGet packages

--- ILSpy/AboutPage.cs
+++ ILSpy/AboutPage.cs
@@ -19,7 +19,6 @@
 using System;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -44,7 +43,7 @@ namespace ICSharpCode.ILSpy
 	{
 		[Import]
 		DecompilerTextView decompilerTextView = null;
-		
+
 		public override void Execute(object parameter)
 		{
 			MainWindow.Instance.UnselectAll();
