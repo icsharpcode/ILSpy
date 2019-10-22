e92b9f9476ec687bd1216e6559402b94e0255dcf
Siegfried Pammer <siegfriedpammer@gmail.com>
2019-10-22 09:32:02 +0200

    Add basic "ugly" test with disabled foreach detection.

---
+++ ICSharpCode.Decompiler.Tests/TestCases/Ugly/NoForEachStatement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ICSharpCode.Decompiler.Tests.TestCases.Ugly
+{
+	internal class NoForEachStatement
+	{
+		public static void SimpleNonGenericForeach(IEnumerable enumerable)
+		{
+			foreach (object item in enumerable) {
+				Console.WriteLine(item);
+			}
+		}
+
+		public static void SimpleForeachOverInts(IEnumerable<int> enumerable)
+		{
+			foreach (int item in enumerable) {
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
