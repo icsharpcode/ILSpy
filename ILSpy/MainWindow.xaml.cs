141440e47fdf81b2811c76683b426e64f6c01399
pvasilek <support.sharebox@gmail.com>
2019-11-27 20:31:48 +0200

    Fixes #1812

--- ILSpy/MainWindow.xaml.cs
+++ ILSpy/MainWindow.xaml.cs
@@ -117,10 +117,10 @@ namespace ICSharpCode.ILSpy
 			};
 
 			DockWorkspace.Instance.LoadSettings(sessionSettings);
-
 			InitializeComponent();
-
 			DockWorkspace.Instance.InitializeLayout(DockManager);
+			DockWorkspace.Instance.PropertyChanged += filter_DockWorkspace_PropertyChanged;
+
 
 			sessionSettings.FilterSettings.PropertyChanged += filterSettings_PropertyChanged;
 
@@ -129,6 +129,17 @@ namespace ICSharpCode.ILSpy
 			ContextMenuProvider.Add(treeView);
 
 			this.Loaded += MainWindow_Loaded;
+		}
+
+		private void filter_DockWorkspace_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == "ActiveDocument") {
+				if ((sender as DockWorkspace).ActiveDocument is DecompiledDocumentModel doc) {
+					var view = doc.TextView;
+					if (view != null)
+						MainWindow.Instance.SelectNodes(view.GetState().DecompiledNodes);
+				}
+			}
 		}
 
 		void SetWindowBounds(Rect bounds)
