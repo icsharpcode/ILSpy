2bb03f556cdaf4bcb7d25c7d232121f72dc7b5a7
Daniel Grunwald <daniel@danielgrunwald.de>
2019-12-04 22:56:14 +0100

    Run nuget restore in the .bat files

--- debugbuild.bat
+++ debugbuild.bat
@@ -1,6 +1,6 @@
 @setlocal enabledelayedexpansion
 @set MSBUILD=
-@for /D %%M in ("%ProgramFiles(x86)%\Microsoft Visual Studio\2019"\*) do (
+@for /D %%M in ("%ProgramFiles(x86)%\Microsoft Visual Studio\2019"\*) do @(
     @if exist "%%M\MSBuild\Current\Bin\MSBuild.exe" (
         @set "MSBUILD=%%M\MSBuild\Current\Bin\MSBuild.exe"
     )
@@ -9,9 +9,5 @@
     @echo Could not find VS2019 MSBuild
     @exit /b 1
 )
-"%MSBUILD%" ILSpy.sln /p:Configuration=Debug "/p:Platform=Any CPU"
-@IF %ERRORLEVEL% NEQ 0 (
-    @pause
-    @exit /b 1
-)
-@exit /b 0
+@nuget restore ILSpy.sln || (pause && exit /b 1)
+"%MSBUILD%" ILSpy.sln /p:Configuration=Debug "/p:Platform=Any CPU" || (pause && exit /b 1)
