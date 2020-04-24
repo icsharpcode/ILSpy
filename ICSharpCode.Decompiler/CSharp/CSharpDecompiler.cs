cd04e80f5eb0c3547c89d34f4824f7fc06d346cb
Daniel Grunwald <daniel@danielgrunwald.de>
2020-04-24 23:23:41 +0200

    Use IsKnownType() when checking for System.Object

--- ICSharpCode.Decompiler/CSharp/CSharpDecompiler.cs
+++ ICSharpCode.Decompiler/CSharp/CSharpDecompiler.cs
@@ -375,7 +375,7 @@ namespace ICSharpCode.Decompiler.CSharp
 				return false;
 			if (name.Contains("DisplayClass") || name.Contains("AnonStorey"))
 				return true;
-			return type.BaseType.GetFullTypeName(metadata).ToString() == "System.Object" && !type.GetInterfaceImplementations().Any();
+			return type.BaseType.IsKnownType(metadata, KnownTypeCode.Object) && !type.GetInterfaceImplementations().Any();
 		}
 		#endregion
 
