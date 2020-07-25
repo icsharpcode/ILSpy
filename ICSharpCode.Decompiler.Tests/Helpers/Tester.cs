a231ab54d4a16e0db88e279b112fddb594a1368c
Siegfried Pammer <siegfriedpammer@gmail.com>
2020-07-25 21:02:43 +0200

    Add test cases

--- ICSharpCode.Decompiler.Tests/Helpers/Tester.cs
+++ ICSharpCode.Decompiler.Tests/Helpers/Tester.cs
@@ -257,6 +257,9 @@ namespace ICSharpCode.Decompiler.Tests.Helpers
 				preprocessorSymbols.Add("LEGACY_CSC");
 				preprocessorSymbols.Add("LEGACY_VBC");
 			}
+			if (flags.HasFlag(CompilerOptions.Preview)) {
+				preprocessorSymbols.Add("CS90");
+			}
 			return preprocessorSymbols;
 		}
 
