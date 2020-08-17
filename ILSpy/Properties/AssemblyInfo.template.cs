eb76ff4090c7e05be257fe96d27e0984182bb341
Christoph Wille <christoph.wille@gmail.com>
2020-08-17 09:12:55 +0200

    Next version will be 6.2-preview1

--- ILSpy/Properties/AssemblyInfo.template.cs
+++ ILSpy/Properties/AssemblyInfo.template.cs
@@ -37,10 +37,10 @@ using System.Diagnostics.CodeAnalysis;
 internal static class RevisionClass
 {
 	public const string Major = "6";
-	public const string Minor = "1";
+	public const string Minor = "2";
 	public const string Build = "0";
 	public const string Revision = "$INSERTREVISION$";
-	public const string VersionName = null;
+	public const string VersionName = "preview1";
 	
 	public const string FullVersion = Major + "." + Minor + "." + Build + ".$INSERTREVISION$$INSERTBRANCHPOSTFIX$$INSERTVERSIONNAMEPOSTFIX$";
 }
