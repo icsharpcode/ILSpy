6824902663f0667c189db209338a0706311a9375
Siegfried Pammer <siegfriedpammer@gmail.com>
2020-08-30 11:47:44 +0200

    Disable formatting of ExpressionTrees.cs, so appveyor does not complain.

--- ICSharpCode.Decompiler.Tests/TestCases/Pretty/ExpressionTrees.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/Pretty/ExpressionTrees.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team
+﻿#pragma warning disable format
+// Copyright (c) AlphaSierraPapa for the SharpDevelop Team
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
