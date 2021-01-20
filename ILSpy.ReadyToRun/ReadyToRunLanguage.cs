2c03e0744b803726754cb2affabf904f7e34aeb3
Andrew Au <cshung@gmail.com>
2021-01-19 20:37:18 -0800

    Update ReadyToRunLanguage.cs

--- ILSpy.ReadyToRun/ReadyToRunLanguage.cs
+++ ILSpy.ReadyToRun/ReadyToRunLanguage.cs
@@ -16,7 +16,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-#define STRESS
+// #define STRESS
 
 using System;
 using System.Collections.Generic;
