2a8333e0678aba60592fd17cf9e492934743a072
Siegfried Pammer <siegfriedpammer@gmail.com>
2021-03-01 21:15:40 +0100

    Fix build.

--- ICSharpCode.Decompiler.Tests/TestAssemblyResolver.cs
+++ ICSharpCode.Decompiler.Tests/TestAssemblyResolver.cs
@@ -13,7 +13,7 @@ namespace ICSharpCode.Decompiler.Tests
 		readonly HashSet<string> localAssemblies = new HashSet<string>();
 
 		public TestAssemblyResolver(string mainAssemblyFileName, string baseDir, string targetFramework)
-			: base(mainAssemblyFileName, false, targetFramework, PEStreamOptions.PrefetchMetadata, MetadataReaderOptions.ApplyWindowsRuntimeProjections)
+			: base(mainAssemblyFileName, false, targetFramework, null, PEStreamOptions.PrefetchMetadata, MetadataReaderOptions.ApplyWindowsRuntimeProjections)
 		{
 			var assemblyNames = new DirectoryInfo(baseDir).EnumerateFiles("*.dll").Select(f => Path.GetFileNameWithoutExtension(f.Name));
 			foreach (var name in assemblyNames)
