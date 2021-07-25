f76acdb09c8851d0b8cb7667d9619baf20a548cc
Andreas Weizel <rpinski@awzhome.de>
2021-07-25 11:00:47 +0200

    Fix: NuGet package nodes were not selected when added through command line

--- ILSpy/MainWindow.xaml.cs
+++ ILSpy/MainWindow.xaml.cs
@@ -379,24 +379,8 @@ namespace ICSharpCode.ILSpy
 
 		List<LoadedAssembly> commandLineLoadedAssemblies = new List<LoadedAssembly>();
 
-		List<string> nugetPackagesToLoad = new List<string>();
-
 		bool HandleCommandLineArguments(CommandLineArguments args)
 		{
-			int i = 0;
-			while (i < args.AssembliesToLoad.Count)
-			{
-				var asm = args.AssembliesToLoad[i];
-				if (Path.GetExtension(asm) == ".nupkg")
-				{
-					nugetPackagesToLoad.Add(asm);
-					args.AssembliesToLoad.RemoveAt(i);
-				}
-				else
-				{
-					i++;
-				}
-			}
 			LoadAssemblies(args.AssembliesToLoad, commandLineLoadedAssemblies, focusNode: false);
 			if (args.Language != null)
 				sessionSettings.FilterSettings.Language = Languages.GetLanguage(args.Language);
@@ -409,13 +393,6 @@ namespace ICSharpCode.ILSpy
 		/// </summary>
 		void HandleCommandLineArgumentsAfterShowList(CommandLineArguments args, ILSpySettings spySettings = null)
 		{
-			if (nugetPackagesToLoad.Count > 0)
-			{
-				var relevantPackages = nugetPackagesToLoad.ToArray();
-				nugetPackagesToLoad.Clear();
-				// Show the nuget package open dialog after the command line/window message was processed.
-				Dispatcher.BeginInvoke(new Action(() => LoadAssemblies(relevantPackages, commandLineLoadedAssemblies, focusNode: false)), DispatcherPriority.Normal);
-			}
 			var relevantAssemblies = commandLineLoadedAssemblies.ToList();
 			commandLineLoadedAssemblies.Clear(); // clear references once we don't need them anymore
 			NavigateOnLaunch(args.NavigateTo, sessionSettings.ActiveTreeViewPath, spySettings, relevantAssemblies);
