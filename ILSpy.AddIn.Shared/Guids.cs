30e2efad9fde08cfcacbc472889fce1bf3915de0
Andreas Weizel <rpinski@awzhome.de>
2021-10-21 00:10:06 +0200

    Introduce a separate package for VS 2022 (VS17) AddIn
    - Set a different GUID for VS 2022 VSIX package
    - Fix wrong path in script

--- ILSpy.AddIn.Shared/Guids.cs
+++ ILSpy.AddIn.Shared/Guids.cs
@@ -6,7 +6,11 @@ namespace ICSharpCode.ILSpy.AddIn
 {
 	static class GuidList
 	{
+#if VS17
+		public const string guidILSpyAddInPkgString = "ebf12ca7-a1fd-4aee-a894-4a0c5682fc2f";
+#else
 		public const string guidILSpyAddInPkgString = "a9120dbe-164a-4891-842f-fb7829273838";
+#endif
 		public const string guidILSpyAddInCmdSetString = "85ddb8ca-a842-4b1c-ba1a-94141fdf19d0";
 
 		public static readonly Guid guidILSpyAddInCmdSet = new Guid(guidILSpyAddInCmdSetString);
---
+++ ILSpy.AddIn.VS17/Decompiler/Dummy.cs
@@ -0,0 +1,30 @@
+﻿// Dummy types so that we can use compile some ICS.Decompiler classes in the AddIn context
+// without depending on SRM etc.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICSharpCode.Decompiler
+{
+	public class ReferenceLoadInfo
+	{
+		public void AddMessage(params object[] args) { }
+	}
+
+	enum MessageKind { Warning }
+
+	public static class MetadataExtensions
+	{
+		public static string ToHexString(this IEnumerable<byte> bytes, int estimatedLength)
+		{
+			if (bytes == null)
+				throw new ArgumentNullException(nameof(bytes));
+
+			StringBuilder sb = new StringBuilder(estimatedLength * 2);
+			foreach (var b in bytes)
+				sb.AppendFormat("{0:x2}", b);
+			return sb.ToString();
+		}
+	}
+}
