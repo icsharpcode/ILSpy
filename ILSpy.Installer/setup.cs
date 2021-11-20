59e1f8e64f31a2dc0f17a0796030133907fedb4b
Andreas Weizel <rpinski@awzhome.de>
2021-11-21 00:22:14 +0100

    Add start menu shortcut on installation

--- ILSpy.Installer/setup.cs
+++ ILSpy.Installer/setup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 using WixSharp;
 
@@ -12,7 +13,7 @@ namespace ILSpy.Installer
 			Compiler.AutoGeneration.IgnoreWildCardEmptyDirectories = true;
 
 			var project = new Project("ILSpy",
-							  new Dir(@"%LocalAppData%\Programs\ILSpy",
+							  new InstallDir(@"%LocalAppData%\Programs\ILSpy",
 								  new DirFiles(@"ILSpy\bin\Release\net472\*.dll"),
 								  new DirFiles(@"ILSpy\bin\Release\net472\*.exe"),
 								  new DirFiles(@"ILSpy\bin\Release\net472\*.config"),
@@ -24,6 +25,7 @@ namespace ILSpy.Installer
 			project.SourceBaseDir = Path.GetDirectoryName(Environment.CurrentDirectory);
 			project.InstallScope = InstallScope.perUser;
 			project.InstallPrivileges = InstallPrivileges.limited;
+			project.ControlPanelInfo.ProductIcon = @"..\ILSpy\Images\ILSpy.ico";
 
 			project.MajorUpgrade = new MajorUpgrade {
 				Schedule = UpgradeSchedule.afterInstallInitialize,
@@ -32,6 +34,11 @@ namespace ILSpy.Installer
 			};
 
 			project.UI = WUI.WixUI_ProgressOnly;
+
+			project.ResolveWildCards().FindFile(f => f.Name.EndsWith("ILSpy.exe")).First()
+				.Shortcuts = new[] {
+					new FileShortcut("ILSpy", @"%ProgramMenu%")
+				};
 
 			Compiler.BuildMsi(project, Path.Combine(Environment.CurrentDirectory, "output", "ILSpy.msi"));
 		}
