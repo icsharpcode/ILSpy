99814773d07bd06a9c30edbbabe15ba084b796ea
Andreas Weizel <rpinski@awzhome.de>
2021-11-20 01:14:56 +0100

    Set up a MSI Builder project with WixSharp creating a basic installer

---
+++ ILSpy.MsiBuilder/setup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using WixSharp;
+
+namespace ILSpy.MsiBuilder
+{
+	internal class MsiBuilder
+	{
+		static public void Main()
+		{
+			Compiler.AutoGeneration.IgnoreWildCardEmptyDirectories = true;
+
+			var project = new Project("ILSpy",
+							  new Dir(@"%LocalAppData%\Programs\ILSpy",
+								  new DirFiles(@"ILSpy\bin\Release\net472\*.dll"),
+								  new DirFiles(@"ILSpy\bin\Release\net472\*.exe"),
+								  new DirFiles(@"ILSpy\bin\Release\net472\*.config"),
+								  new Files(@"ILSpy\bin\Release\net472\ILSpy.resources.dll"),
+								  new Files(@"ILSpy\bin\Release\net472\ILSpy.ReadyToRun.Plugin.resources.dll")));
+
+			project.GUID = new Guid("a12fdab1-731b-4a98-9749-d481ce8692ab");
+			project.Version = new Version("1.0.0.0");
+			project.SourceBaseDir = Path.GetDirectoryName(Environment.CurrentDirectory);
+			project.InstallScope = InstallScope.perUser;
+			project.InstallPrivileges = InstallPrivileges.limited;
+
+			project.UI = WUI.WixUI_Minimal;
+
+			Compiler.BuildMsi(project, Path.Combine(Environment.CurrentDirectory, "output", "ILSpy.msi"));
+		}
+	}
+}
