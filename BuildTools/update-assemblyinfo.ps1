c7719b1a6f48b9ed24909205777956168ac4a745
Siegfried Pammer <siegfriedpammer@gmail.com>
2022-04-03 16:52:51 +0200

    Try again to remove ILSpy.dll.config.

--- BuildTools/update-assemblyinfo.ps1
+++ BuildTools/update-assemblyinfo.ps1
@@ -80,7 +80,6 @@ function gitBranch() {
 
 $templateFiles = (
     @{Input=$decompilerVersionInfoTemplateFile; Output="ICSharpCode.Decompiler/Properties/DecompilerVersionInfo.cs"},
-    @{Input="ILSpy/Properties/app.config.template"; Output = "ILSpy/app.config"},
     @{Input="ILSpy.AddIn/source.extension.vsixmanifest.template"; Output = "ILSpy.AddIn/source.extension.vsixmanifest"},
     @{Input="ILSpy.AddIn.VS2022/source.extension.vsixmanifest.template"; Output = "ILSpy.AddIn.VS2022/source.extension.vsixmanifest"},
     @{Input="ILSpy.Installer/AppPackage.cs.template"; Output = "ILSpy.Installer/AppPackage.cs"}
