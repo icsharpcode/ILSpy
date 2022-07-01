760a6a653e4d7e30001568dbc7201c25ec3cfb78
ElektroKill <elektrokilldev@protonmail.com>
2022-07-01 18:45:46 +0200

    Reduce allocations in TransformArrayInitializers
    DecodeArrayInitializer - Instead of relying on the Add method of a list to expand the underlying array when necessary, the code now allocates a big enough array to fit all the elements removing the need for the Add method to expand the array several times.
    
    BlockFromInitializer now reuses a single instance of List<ILInstruction> instead of reallocating a new one every time and clears it when necessary. The same pre-allocation approach mentioned above has been implemented here too.

--- ICSharpCode.Decompiler/IL/Transforms/TransformArrayInitializers.cs
+++ ICSharpCode.Decompiler/IL/Transforms/TransformArrayInitializers.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2015 Siegfried Pammer
+// Copyright (c) 2015 Siegfried Pammer
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
@@ -620,16 +620,21 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 			var block = new Block(BlockKind.ArrayInitializer);
 			block.Instructions.Add(new StLoc(v, new NewArr(elementType, arrayLength.Select(l => new LdcI4(l)).ToArray())));
 			int step = arrayLength.Length + 1;
+
+			var indices = new List<ILInstruction>();
 			for (int i = 0; i < values.Length / step; i++)
 			{
 				// values array is filled backwards
 				var value = values[step * i];
-				var indices = new List<ILInstruction>();
+
+				indices.EnsureCapacity(step - 1);
 				for (int j = step - 1; j >= 1; j--)
 				{
 					indices.Add(values[step * i + j]);
 				}
+
 				block.Instructions.Add(StElem(new LdLoc(v), indices.ToArray(), value, elementType));
+				indices.Clear();
 			}
 			block.FinalInstruction = new LdLoc(v);
 			return block;
@@ -773,6 +778,8 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 			var totalLength = arrayLength.Aggregate(1, (t, l) => t * l);
 			if (initialValue.RemainingBytes < (totalLength * elementSize))
 				return false;
+
+			output.EnsureCapacity(totalLength + totalLength * arrayLength.Length);
 
 			for (int i = 0; i < totalLength; i++)
 			{
