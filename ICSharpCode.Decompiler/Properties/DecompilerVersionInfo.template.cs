b6766d906abd7cbbc8da257bb5c7fc0c74e90861
Christoph Wille <christoph.wille@gmail.com>
2022-07-18 07:11:48 +0200

    8.0.0-preview2 will be next version

--- ICSharpCode.Decompiler/Properties/DecompilerVersionInfo.template.cs
+++ ICSharpCode.Decompiler/Properties/DecompilerVersionInfo.template.cs
@@ -4,7 +4,7 @@
 	public const string Minor = "0";
 	public const string Build = "0";
 	public const string Revision = "$INSERTREVISION$";
-	public const string VersionName = "preview1";
+	public const string VersionName = "preview2";
 
 	public const string FullVersion = Major + "." + Minor + "." + Build + ".$INSERTREVISION$$INSERTBRANCHPOSTFIX$$INSERTVERSIONNAMEPOSTFIX$";
 	public const string FullVersionWithShortCommitHash = FullVersion + "-$INSERTSHORTCOMMITHASH$";
