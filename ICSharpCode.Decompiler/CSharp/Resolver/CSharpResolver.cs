1447038ce1a43f612b8ded7bba195a3c6066c8a9
ElektroKill <elektrokilldev@protonmail.com>
2022-09-18 20:16:27 +0200

    Fix #2736

--- ICSharpCode.Decompiler/CSharp/Resolver/CSharpResolver.cs
+++ ICSharpCode.Decompiler/CSharp/Resolver/CSharpResolver.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2010-2013 AlphaSierraPapa for the SharpDevelop Team
+// Copyright (c) 2010-2013 AlphaSierraPapa for the SharpDevelop Team
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
@@ -1760,7 +1760,7 @@ namespace ICSharpCode.Decompiler.CSharp.Resolver
 			}
 			// then look for a type
 			ITypeDefinition def = n.GetTypeDefinition(identifier, k);
-			if (def != null)
+			if (def != null && TopLevelTypeDefinitionIsAccessible(def))
 			{
 				IType result = def;
 				if (parameterizeResultType && k > 0)
