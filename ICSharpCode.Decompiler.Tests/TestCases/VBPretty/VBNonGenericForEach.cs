02cf1d3fe27334c2c623581e193c0d0b0582a8e6
ElektroKill <elektrokilldev@protonmail.com>
2022-11-19 21:01:17 +0100

    Add unit test

---
+++ ICSharpCode.Decompiler.Tests/TestCases/VBPretty/VBNonGenericForEach.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+public class VBNonGenericForEach
+{
+	public static void M()
+	{
+		ArrayList arrayList = new ArrayList();
+		foreach (object item in arrayList)
+		{
+#if ROSLYN && OPT
+			Console.WriteLine(RuntimeHelpers.GetObjectValue(RuntimeHelpers.GetObjectValue(item)));
+#else
+			object objectValue = RuntimeHelpers.GetObjectValue(item);
+			Console.WriteLine(RuntimeHelpers.GetObjectValue(objectValue));
+#endif
+		}
+	}
+}
