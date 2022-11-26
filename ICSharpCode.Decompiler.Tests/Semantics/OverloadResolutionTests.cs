119d8df4316d7ec20974b8ee41d13a57ed9a2dd5
ElektroKill <elektrokilldev@protonmail.com>
2022-11-26 14:18:02 +0100

    Enable two `OverloadResolution` tests as they are no longer broken

--- ICSharpCode.Decompiler.Tests/Semantics/OverloadResolutionTests.cs
+++ ICSharpCode.Decompiler.Tests/Semantics/OverloadResolutionTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2010-2013 AlphaSierraPapa for the SharpDevelop Team
+// Copyright (c) 2010-2013 AlphaSierraPapa for the SharpDevelop Team
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
@@ -104,7 +104,7 @@ namespace ICSharpCode.Decompiler.Tests.Semantics
 			Assert.AreSame(c1, r.BestCandidate);
 		}
 
-		[Test, Ignore("Broken after migration to ICS.Decompiler")]
+		[Test]
 		public void NullableIntAndNullableUIntIsAmbiguous()
 		{
 			OverloadResolution r = new OverloadResolution(compilation, MakeArgumentList(typeof(ushort?)));
@@ -300,7 +300,7 @@ namespace ICSharpCode.Decompiler.Tests.Semantics
 			Assert.AreEqual(OverloadResolutionErrors.None, r.BestCandidateErrors);
 		}
 
-		[Test, Ignore("Broken on SRM branch???")]
+		[Test]
 		public void Lambda_DelegateAndExpressionTreeOverloadsAreAmbiguous()
 		{
 			var m1 = MakeMethod(typeof(Func<int>));
