7352478bc8db435a7d3b2512970ceb1031bcdde1
Christoph Wille <christoph.wille@gmail.com>
2022-12-15 09:32:23 +0100

    Minimal generic host

---
+++ ILSpy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ICSharpCode.ILSpy
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+			// https://learn.microsoft.com/en-us/dotnet/core/extensions/generic-host
+			var host = Host.CreateDefaultBuilder()
+				.ConfigureServices(services => {
+					services.AddSingleton<App>();
+				})
+				.Build();
+
+			var app = host.Services.GetService<App>();
+			app.Run();
+		}
+	}
+}
