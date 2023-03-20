ef9aa46cecc0549fa5dbfc460b617372b8d5340f
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2023-03-20 22:27:37 +0100

    Add theme menu item

---
+++ ILSpy/Commands/SetThemeCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace ICSharpCode.ILSpy.Commands
+{
+	public class SetThemeCommand : SimpleCommand
+	{
+		public override void Execute(object parameter)
+		{
+			if (parameter is string theme)
+				MainWindow.Instance.SessionSettings.Theme = theme;
+		}
+	}
+}
