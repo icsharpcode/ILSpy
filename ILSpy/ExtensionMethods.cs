ba58ecf0171ae9ab2142dff6f19464d45c42cd85
tom-englert <mail@tom-englert.de>
2023-04-05 16:54:48 +0200

    Alternate approach to #2947

--- ILSpy/ExtensionMethods.cs
+++ ILSpy/ExtensionMethods.cs
@@ -161,5 +161,10 @@ namespace ICSharpCode.ILSpy
 				container.IsSelected = true;
 			view.Focus();
 		}
+
+		public static double ToGray(this Color? color)
+		{
+			return color?.R * 0.3 + color?.G * 0.6 + color?.B * 0.1 ?? 0.0;
+		}
 	}
 }
