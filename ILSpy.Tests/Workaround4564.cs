3983187f36b63bb02676127b7ed1d60e2fd8cde9
Christoph Wille <christoph.wille@gmail.com>
2023-11-28 18:27:23 +0100

    Try adding TargetPlatform too

--- ILSpy.Tests/Workaround4564.cs
+++ ILSpy.Tests/Workaround4564.cs
@@ -1,3 +1,4 @@
 ﻿// https://github.com/nunit/nunit/issues/4564
 
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("Windows10")]
+[assembly: System.Runtime.Versioning.TargetPlatform("Windows10")]
