d3de4745048da6a4ae59694625930d02ab51cdb5
Christoph Wille <christoph.wille@gmail.com>
2023-11-29 09:41:10 +0100

    Test 4.1.0-adev.1 from NUnit MyGet feed (works locally in VS)

--- ILSpy.Tests/Workaround4564.cs
+++
@@ -1,4 +0,0 @@
-﻿// https://github.com/nunit/nunit/issues/4564
-
-[assembly: System.Runtime.Versioning.SupportedOSPlatform("Windows10")]
-[assembly: System.Runtime.Versioning.TargetPlatform("Windows10")]
