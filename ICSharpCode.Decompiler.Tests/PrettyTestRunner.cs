d75da053a8c31070629e993d3577cbb9cb954ec5
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-02-25 14:03:59 -0800

    Use type hint in pointer arithmetic when appropriate

--- ICSharpCode.Decompiler.Tests/PrettyTestRunner.cs
+++ ICSharpCode.Decompiler.Tests/PrettyTestRunner.cs
@@ -730,6 +730,12 @@ namespace ICSharpCode.Decompiler.Tests
 			await RunForLibrary(cscOptions: cscOptions);
 		}
 
+		[Test]
+		public async Task PointerArithmetic([ValueSource(nameof(defaultOptions))] CompilerOptions cscOptions)
+		{
+			await RunForLibrary(cscOptions: cscOptions);
+		}
+
 		async Task RunForLibrary([CallerMemberName] string testName = null, AssemblerOptions asmOptions = AssemblerOptions.None, CompilerOptions cscOptions = CompilerOptions.None, Action<DecompilerSettings> configureDecompiler = null)
 		{
 			await Run(testName, asmOptions | AssemblerOptions.Library, cscOptions | CompilerOptions.Library, configureDecompiler);
---
+++ ICSharpCode.Decompiler.Tests/TestCases/Pretty/PointerArithmetic.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
+{
+	public class PointerArithmetic
+	{
+		public unsafe static void AssignmentVoidPointerToIntPointer(void* ptr)
+		{
+			((int*)ptr)[2] = 1;
+		}
+
+		public unsafe static int AccessVoidPointerToIntPointer(void* ptr)
+		{
+			return ((int*)ptr)[2];
+		}
+
+		public unsafe static void AssignmentLongPointerToIntPointer_2(long* ptr)
+		{
+			((int*)ptr)[2] = 1;
+		}
+
+		public unsafe static int AccessLongPointerToIntPointer_2(long* ptr)
+		{
+			return ((int*)ptr)[2];
+		}
+
+		public unsafe static void AssignmentLongPointerToIntPointer_3(long* ptr)
+		{
+			((int*)ptr)[3] = 1;
+		}
+
+		public unsafe static int AccessLongPointerToIntPointer_3(long* ptr)
+		{
+			return ((int*)ptr)[3];
+		}
+
+		public unsafe static void AssignmentGuidPointerToIntPointer(Guid* ptr)
+		{
+			((int*)ptr)[2] = 1;
+		}
+
+		public unsafe static int AccessGuidPointerToIntPointer(Guid* ptr)
+		{
+			return ((int*)ptr)[2];
+		}
+
+		public unsafe static void AssignmentIntPointer(int* ptr)
+		{
+			ptr[2] = 1;
+		}
+
+		public unsafe static int AccessIntPointer(int* ptr)
+		{
+			return ptr[2];
+		}
+
+		public unsafe static void AssignmentGuidPointer(Guid* ptr)
+		{
+			ptr[2] = Guid.NewGuid();
+		}
+
+		public unsafe static Guid AccessGuidPointer(Guid* ptr)
+		{
+			return ptr[2];
+		}
+
+		public unsafe static void AssignmentVoidPointerToGuidPointer(void* ptr)
+		{
+			((Guid*)ptr)[2] = Guid.NewGuid();
+		}
+
+		public unsafe static Guid AccessVoidPointerToGuidPointer(void* ptr)
+		{
+			return ((Guid*)ptr)[2];
+		}
+
+		public unsafe static void AssignmentIntPointerToGuidPointer(int* ptr)
+		{
+			((Guid*)ptr)[2] = Guid.NewGuid();
+		}
+
+		public unsafe static Guid AccessIntPointerToGuidPointer(int* ptr)
+		{
+			return ((Guid*)ptr)[2];
+		}
+	}
+}
