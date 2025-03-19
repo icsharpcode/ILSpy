49942382d17fdeb17cdbd42ea9222599fddf1e47
Siegfried Pammer <siegfriedpammer@gmail.com>
2025-03-19 22:52:27 +0100

    Fix #3432: Do not include delegate construction use-sites in DetermineCaptureAndDeclarationScopes.

--- ICSharpCode.Decompiler/IL/Transforms/LocalFunctionDecompiler.cs
+++ ICSharpCode.Decompiler/IL/Transforms/LocalFunctionDecompiler.cs
@@ -658,6 +658,12 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 			switch (useSite)
 			{
 				case CallInstruction call:
+					if (DelegateConstruction.MatchDelegateConstruction(useSite, out _, out _, out _))
+					{
+						// if this is a delegate construction, skip the use-site, because the capture scope
+						// was already determined when analyzing "this".
+						break;
+					}
 					int firstArgumentIndex = info.Definition.Method.IsStatic ? 0 : 1;
 					for (int i = call.Arguments.Count - 1; i >= firstArgumentIndex; i--)
 					{
