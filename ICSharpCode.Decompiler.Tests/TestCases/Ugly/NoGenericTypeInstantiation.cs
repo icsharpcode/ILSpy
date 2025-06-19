caea72824d53d90e12a109453708135136fdc01c
DoctorKrolic <mapmyp03@gmail.com>
2025-06-19 19:49:24 +0300

    Add an option to not transform `Activator.CreateInstance<T>()` to `new T()`

---
+++ ICSharpCode.Decompiler.Tests/TestCases/Ugly/NoGenericTypeInstantiation.cs
@@ -0,0 +1,15 @@
+﻿namespace ICSharpCode.Decompiler.Tests.TestCases.Ugly
+{
+	internal class NoGenericTypeInstantiation<TOnType> where TOnType : new()
+	{
+		public static TOnType CreateTOnType()
+		{
+			return new TOnType();
+		}
+
+		public static TOnMethod CreateTOnMethod<TOnMethod>() where TOnMethod : new()
+		{
+			return new TOnMethod();
+		}
+	}
+}
