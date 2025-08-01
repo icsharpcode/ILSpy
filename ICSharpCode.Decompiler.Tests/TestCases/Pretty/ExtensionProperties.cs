ffb8299ea2d65f287d34fefe8c50a4b665cda1df
Siegfried Pammer <siegfriedpammer@gmail.com>
2025-08-01 19:28:19 +0200

    Test case

---
+++ ICSharpCode.Decompiler.Tests/TestCases/Pretty/ExtensionProperties.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
+{
+	static class ExtensionProperties
+	{
+		extension<T>(ICollection<T> collection)
+		{
+			public bool IsEmpty => collection.Count == 0;
+	}
+}
+}
