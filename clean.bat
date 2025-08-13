66c054a6200d9ab59288ec6a8ef45565c3086e5f
Christoph Wille <christoph.wille@gmail.com>
2025-08-13 18:31:14 +0200

    Fix up clean.bat as well to use dotnet

--- clean.bat
+++ clean.bat
@@ -1,13 +1,2 @@
-@setlocal enabledelayedexpansion
-@set MSBUILD=
-@for /D %%M in ("%ProgramFiles%\Microsoft Visual Studio\2022"\*) do (
-    @if exist "%%M\MSBuild\Current\Bin\MSBuild.exe" (
-        @set "MSBUILD=%%M\MSBuild\Current\Bin\MSBuild.exe"
-    )
-)
-@if "%MSBUILD%" == "" (
-    @echo Could not find VS2022 MSBuild
-    @exit /b 1
-)
-"%MSBUILD%" /m ILSpy.sln /t:Clean /p:Configuration=Debug "/p:Platform=Any CPU" || pause
-"%MSBUILD%" /m ILSpy.sln /t:Clean /p:Configuration=Release "/p:Platform=Any CPU" || pause
+dotnet clean ILSpy.sln /p:Configuration=Debug "/p:Platform=Any CPU" %* || pause
+dotnet clean ILSpy.sln /p:Configuration=Release "/p:Platform=Any CPU" %* || pause
