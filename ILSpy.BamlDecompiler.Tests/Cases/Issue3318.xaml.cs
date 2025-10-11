995d31fc2d56be836fd4f20d242c44c3dfd7661e
Siegfried Pammer <siegfriedpammer@gmail.com>
2025-10-11 10:03:32 +0200

    Fix #3318: missing x:FieldModifier in BAML decompiler

---
+++ ILSpy.BamlDecompiler.Tests/Cases/Issue3318.xaml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ILSpy.BamlDecompiler.Tests.Cases
+{
+	/// <summary>
+	/// Interaction logic for Issue3318.xaml
+	/// </summary>
+	public partial class Issue3318 : UserControl
+	{
+		public Issue3318()
+		{
+			InitializeComponent();
+		}
+	}
+}
