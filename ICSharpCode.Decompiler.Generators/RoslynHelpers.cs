217f15d670cd2601aba05c1af9f4f815b78db2e8
Siegfried Pammer <siegfriedpammer@gmail.com>
2026-06-19 08:52:05 +0200

    Remove dead generator helpers: IsTypeNode, GetTopLevelTypes, TreeTraversal

--- ICSharpCode.Decompiler.Generators/RoslynHelpers.cs
+++ ICSharpCode.Decompiler.Generators/RoslynHelpers.cs
@@ -4,17 +4,6 @@ namespace ICSharpCode.Decompiler.Generators;
 
 public static class RoslynHelpers
 {
-	public static IEnumerable<INamedTypeSymbol> GetTopLevelTypes(this IAssemblySymbol assembly)
-	{
-		foreach (var ns in TreeTraversal.PreOrder(assembly.GlobalNamespace, ns => ns.GetNamespaceMembers()))
-		{
-			foreach (var t in ns.GetTypeMembers())
-			{
-				yield return t;
-			}
-		}
-	}
-
 	public static bool IsDerivedFrom(this INamedTypeSymbol type, INamedTypeSymbol baseType)
 	{
 		INamedTypeSymbol? t = type;
