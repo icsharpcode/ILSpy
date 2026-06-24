aab6303870dbd439a5c9368287c5dd97860a2580
Christoph Wille <christoph.wille@gmail.com>
2026-06-24 06:09:29 +0200

    Migrate the VS extension to an SDK-style VSIX (dotnet build) and retire the VS2017/2019 add-in (#3808)
    * Migrate ILSpy.AddIn.VS2022 to SDK-style VSIX project
    * Retire the legacy VS2017/2019 ILSpy add-in
    * Build the VS extension via dotnet build on the slnx
    * Inline ILSpy.AddIn.Shared into ILSpy.AddIn.VS2022
    * Restore the VS add-in menus broken by the SDK-style migration

--- ILSpy.AddIn.VS2022/ILSpyAddInPackage.cs
+++ ILSpy.AddIn.VS2022/ILSpyAddInPackage.cs
@@ -36,7 +36,7 @@ namespace ICSharpCode.ILSpy.AddIn
 	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 	// This attribute is used to register the information needed to show this package
 	// in the Help/About dialog of Visual Studio.
-	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[InstalledProductRegistration("#110", "#112", "1.0")]
 	// This attribute is needed to let the shell know that this package exposes some menus.
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	[Guid(GuidList.guidILSpyAddInPkgString)]
